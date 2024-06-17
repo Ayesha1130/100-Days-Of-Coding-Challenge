0896310410308607853be89379ff16b1f46e535a
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-06-17 15:26:09 +0500

    main

--- Day 40/main.ts
+++ Day 40/main.ts
@@ -4,6 +4,7 @@ Write a loop that logs numbers from 1 to 10 to the console.
 
 Explain & TIP: A for loop is a concise way to run a block of code a specific number of times. It's perfect for when you know exactly how many iterations you need.*/
 
+// Example of a loop that logs numbers from 1 to 10 to the console
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
@@ -14,7 +15,7 @@ Create a while loop that logs "Hello, World!" 5 times.
 
 Explain & TIP: A while loop continues to run as long as its condition remains true. It’s ideal for when you want to repeat something until a certain condition changes.*/
 
-//let name: string = "Hello, World!"
+// Create a while loop that logs "Hello world!" 5 times
 let count: number = 0;
 while(count < 5){
     console.log("Hello World");
@@ -25,6 +26,7 @@ while(count < 5){
 Use a for...of loop to iterate through an array of your favorite movies and log each one.
 
 Explain & TIP: The for...of loop is a modern loop introduced in ES6, designed to iterate over iterable objects like arrays, strings, Maps, NodeLists, and more, making it very readable and easy to use.*/
+
 
 let movies: string[] = [ "Inception",
     "The Matrix",
